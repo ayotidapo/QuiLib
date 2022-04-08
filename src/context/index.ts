2e34af6dc8f1afa699f1b1e13500d2d5765a4134
otidapson <oladapo.o@softcom.ng>
2022-04-08 06:00:04 +0100

    remove cached

--- src/context/index.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-import { Book } from 'interfaces';
-
-export const BookContext = createContext<Book>({
-	addedBooks:[],
-	books:[],
-	searchedBooks: [],
-	featured:[]
-});
--- src/interfaces/index.ts
+++
@@ -1,4 +0,0 @@
-export interface Book{
-	[key:string]:any;
-
-}
