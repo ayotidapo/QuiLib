758c4b86c6ad3bdcd896d85a07fe29d7f0aadde9
otidapson <oladapo.o@softcom.ng>
2022-04-08 04:12:50 +0100

    change gitignorefile directory

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,50 +1,34 @@
-import { useEffect } from 'react'
-import Flickity from "flickity"
-import 'flickity/dist/flickity.min.css';
 import './carousel.css'
 
+import Flickity from 'react-flickity-component'
 
+const flickityOptions = {
+	initialIndex: 3,
+	wrapAround: true
+}
 
-const Carousel: React.FC = () => {
-
-	useEffect(() => {
-
-		new Flickity('.main-carousel', {
-			autoPlay: 4000,
-			pauseAutoPlayOnHover: true,
-			groupCells: true,
-			wrapAround: true,
-			adaptiveHeight: true,
-			freeScroll: true,
-			cellAlign: 'left',
-		});
-
-		// const  elem = document.querySelector('.main-carousel');
-		// const flkty = new Flickity( elem as Element, {
-		// // options
-		// 	cellAlign: 'left',
-		// 	contain: true
-		// })
-
-	}, []);
-
-
+function Carousel() {
 	return (
-
-
 		<section className="carousel-wrapper">
-			<div className=".main-carousel">
-				<div className="carousel-cell" > <img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
-				<div className="carousel-cell"> <img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
-				<div className="carousel-cell"> <img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
-				<div className="carousel-cell" > <img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
-				<div className="carousel-cell"> <img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
-				<div className="carousel-cell"> <img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
-
-
-			</div>
-
-
+			<Flickity
+				className={'carousel'} // default ''
+				elementType={'div'} // default 'div'
+				options={flickityOptions} // takes flickity options {}
+				disableImagesLoaded={false} // default false
+				reloadOnUpdate // default false
+				static // default false
+			>
+				<div className="gallery-cell"><img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
+				<div className="gallery-cell"><img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
+				<div className="gallery-cell"><img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
+				<div className="gallery-cell"><img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
+				<div className="gallery-cell"><img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
+				<div className="gallery-cell"><img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
+				<div className="gallery-cell"><img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
+				<div className="gallery-cell"><img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
+				<div className="gallery-cell"><img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
+				<div className="gallery-cell"><img src="https://res.cloudinary.com/quidaxengineering/image/upload/v1611741483/feec/the-effective-engineer-cover_bgj7u4.jpg" alt="" /></div>
+			</Flickity>
 		</section>
 	)
 }
